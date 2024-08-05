1fe7cf7206efa80540d5877f9085e2ee498175ce
niehang <niehang@gmail.com>
2024-08-05 14:04:22 +0800

    ci: auto import antDesign and vue and vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,34 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: true
+      }
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false
+        })
+      ]
+    })
   ],
   resolve: {
     alias: {
