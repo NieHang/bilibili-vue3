ad8a4e9ea83879a516359fe7f8d3db9be9573b0c
niehang <niehang@gmail.com>
2024-08-08 10:41:06 +0800

    ci: add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,14 @@ export default defineConfig({
       ]
     })
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html']
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
